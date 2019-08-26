98d5237da4431a7dc9b74c1f8fc9fc250d73dd57
marshallking <marshallking@comcast.net>
2019-08-26 07:27:56 -0700

    Updating main.go
    Chanced hello to ballard hello/.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello King World"}
+		res := &response{Message: "Hello Ballard World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
