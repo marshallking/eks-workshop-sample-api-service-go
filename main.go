2c1f0cc49a9a34d60adca300fc78a6e41f78bdc4
marshallking <marshallking@comcast.net>
2019-08-28 13:52:55 -0700

    update main.go
    enumclaw the best

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Buckley World"}
+		res := &response{Message: "Hello Enumclaw World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
